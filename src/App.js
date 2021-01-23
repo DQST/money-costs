5c56a3be57523a362543769c41581128029c1dc6
Dmitriy Yushko <d.yushko@itexus.com>
2021-01-23 22:38:27 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { App, View } from 'framework7-react';
+import { MainPage } from 'pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default () => (
+  <App theme="auto" name="Money Costs" id="com.demoapp.test">
+    <View main>
+      <MainPage/>
+    </View>
+  </App>
+)
--- src/index.js
+++ src/index.js
@@ -5,17 +5,25 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// Import F7 Bundle
+import Framework7 from 'framework7/lite-bundle';
+
+// Import F7-React Plugin
+import Framework7React from 'framework7-react';
+import 'framework7/framework7-bundle.min.css';
+
+// Init F7-React Plugin
+Framework7.use(Framework7React);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement(App),
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
