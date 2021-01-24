1bbfb092fd512e032a48726cfef5e35953636c95
Dmitriy Yushko <d.yushko@itexus.com>
2021-01-25 01:23:22 +0300

    Added ModalSheet

---
+++ src/components/ModalSheet/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Sheet, Toolbar, Link, PageContent } from 'framework7-react';
+import { IoCloseCircleOutline } from 'react-icons/io5';
+
+
+const ModalSheet = ({id, children, position = 'bottom'}) => (
+    <Sheet
+        backdrop
+        swipeToClose
+        position={position}
+        className={id}
+    >
+        <Toolbar>
+            <div></div>
+            <div>
+                <Link sheetClose={`.${id}`}>
+                    <IoCloseCircleOutline size="30px"/>
+                </Link>
+            </div>
+        </Toolbar>
+        <PageContent>
+            {children}
+        </PageContent>
+    </Sheet>
+);
+
+export default ModalSheet;
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Avatar from "./Avatar";
 import Wallet from "./Wallet";
 import TransactionList from "./TransactionList";
 import Keypad from "./Keypad";
+import ModalSheet from './ModalSheet';
 
 export {
     Avatar,
     Wallet,
     TransactionList,
     Keypad,
+    ModalSheet,
 }
