43762260ec88fc25cd00b920612282f9822d4fe0
Dmitriy Yushko <d.yushko@itexus.com>
2021-01-25 21:35:26 +0300

    Updated db

--- src/db/index.js
+++ src/db/index.js
@@ -2,20 +2,20 @@ import Dexie from "dexie";
 import { DefaultUser, Currencies, CostsCategories } from './consts';
 
 const db = new Dexie('money-costs');
-db.version(1).stores({
+db.version(3).stores({
   user: '++id',
   currency: '++id',
   category: '++id',
-  wallet: '++id, user_id, currency_id, created_at, updated_at',
-  transaction: '++id, currency_id, wallet_id, timestamp',
+  wallet: '++id, user_id, created_at, updated_at',
+  walletTransactions: '++id, wallet_id, created_at, updated_at',
   report: '++id, wallet_id, created_at',
-  report_data: '++id, report_id, transaction_id',
+  reportData: '++id, report_id, transaction_id',
 });
 
 async function initDb() {
   const isUserExists = await db.user.count();
   if (!isUserExists) {
-    db.user.add(DefaultUser);
+    await db.user.add(DefaultUser);
   }
 
   const costsCategories = await db.category.count();
