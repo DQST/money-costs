8395354ca3025779145a056a92ed7178c25d56fa
Dmitriy Yushko <d.yushko@itexus.com>
2021-01-25 23:08:29 +0300

    Removed Keypad component

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,11 @@
 import Avatar from "./Avatar";
 import Wallet from "./Wallet";
 import TransactionList from "./TransactionList";
-import Keypad from "./Keypad";
 import ModalSheet from './ModalSheet';
 
 export {
     Avatar,
     Wallet,
     TransactionList,
-    Keypad,
     ModalSheet,
 }
